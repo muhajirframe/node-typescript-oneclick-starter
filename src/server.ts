4aa9ce2fcbba923bc04f97cb910f24e2a87c06c1
Muhammad Muhajir <muhajirframe@gmail.com>
2018-10-25 17:41:28 +0700

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express'
+import { logger } from './logger'
 
 const app = express()
 
 const port = 3000
 
 app.get('/', (_, res) => {
+  logger.info('Got a request')
   res.send('Hello world')
 })
 
-app.listen(port)
+app.listen(port, () => {
+  logger.info(`listening on port ${port}`)
+})
